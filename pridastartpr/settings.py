f0277b91fdb6424140c83cb3ea66e1d6cf5315a6
tihobg <66679938+tihobg@users.noreply.github.com>
2024-11-05 13:34:17 +0200

    Update settings.py
    commit update settings 2

--- pridastartpr/settings.py
+++ pridastartpr/settings.py
@@ -75,8 +75,7 @@ WSGI_APPLICATION = 'pridastartpr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = 
-{
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
